39ef6d0224bc79e4ae800050804c96c640f5352e
MBenincasa <m.benincasa13@studenti.unisa.it>
2020-08-31 11:16:49 +0200

    Commit di prova dopo la creazione di un model

---
+++ src/main/java/com/example/simpleforum/model/Utente.java
@@ -0,0 +1,13 @@
+package com.example.simpleforum.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "utente")
+public class Utente {
+
+}
